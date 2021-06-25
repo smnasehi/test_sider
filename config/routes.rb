51221b46ddc05d9b431c7830ed72597e296a5eb4
Seyed Nasehi <snasehi@cisco.com>
2021-06-25 15:41:45 -0600

    Test a new rule

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'credit_cards/open'
-  get 'credit_cards/debit'
-  get 'credit_cards/credit'
+  resources :credit_cards do
+    member do
+      get :debit
+      get :credit
+    end
+    collection do
+      post :open
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
