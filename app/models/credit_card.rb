1a462c932ec8f234a81027b05cb523ffe56c9fe1
Seyed Nasehi <snasehi@cisco.com>
2021-06-26 16:21:59 -0600

    Add config files

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,6 +1,8 @@
 class CreditCard < ApplicationRecord
 
-  def bad_method(name, number, expiry_date, some_other_stuff)
+  @@class_var = 1
+
+  def bad_method(name, number, expiry_date, some_other_stuff, another_param, and_another)
     y = 10
   end
 end
